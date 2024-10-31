6e1befb43524b9b8b5fabf97a3daa679dc1a7739
yadicamila <yadimadronero@unicomfacauca.edu.co>
2024-10-30 23:51:52 -0500

    first commit

---
+++ ejerciciosTailwind/tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./*.html"], // Asegúrate de incluir todos los archivos HTML en los que usarás Tailwind
+  theme: {
+    extend: {
+      colors: {
+        primary: '#302c25', 
+        secondary: '#c21f40', 
+        accent: '#F59E0B',  
+        background: '#fafafa',
+        textPrimary: '#111827',
+        textSecondary: '#6B7280',
+
+      },
+    },
+  },
+};
+
